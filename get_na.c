3d8e93d413224b2a91580b35bf33af2407d58285
Ahlam Raiteb <araiteb@e2r8p5.1337.ma>
2022-11-13 19:18:48 +0100

    get_next_line modi

--- get_na.c
+++ get_na.c
@@ -6,21 +6,45 @@
 /*   By: araiteb <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 16:23:28 by araiteb           #+#    #+#             */
-/*   Updated: 2022/11/13 16:31:45 by araiteb          ###   ########.fr       */
+/*   Updated: 2022/11/13 17:53:50 by araiteb          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "get_next_line.h"
+static char *remain;
+
 char *get_line(int fd,char *line)
 {
 	char *str;
-	
+	int i;
+	int len;
+	char *ret;
 	if (!line)
 		return(NULL);
-	str = ft_strdup(line);
-	while(!ft_strchr(line,'\n') && (i = read(fd,line,BUFFER_SIZE) > 0))
+	if(remain)
 	{
-		
+		str = ft_strdup(remain);
+		str = ft_strjoin(str,line);
+	}
+	else
+		str = ft_strdup(line);
+	while(!ft_strchr(line,'\n') && (i = read(fd,line,BUFFER_SIZE)) > 0)
+	{
+		str = ft_strjoin(str,line);
+	}
+	if(ft_strchr(str,'\n'))
+	{
+		len = ft_strlen(str)- ft_strlen(ft_strchr(str,'\n'));
+		ret = ft_substr(str,0,len+1);
+		remain = ft_substr(str,len+1,ft_strlen(str));
+		free(str);
+		free(line);
+		return(ret);
+	}
+	else  //if( i == 0)
+	{
+		//printf("line -->%s",line);
+		return(str);
 	}
 }
 char *get_next_line(int fd)
@@ -28,8 +52,32 @@ char *get_next_line(int fd)
 	char *line;
 	char *rev;
 	int i;
-	if(fd < 0 || i = read(fd,line,BUFFER_SIZE) < 0)
-		return(NULL);
-	rev = get_line;
+	line = ft_calloc((BUFFER_SIZE+1),sizeof(char));
+	
+	if( fd < 0 || (i = read(fd,line,BUFFER_SIZE) <= 0 ))
+		return(NULL); 
+	//printf("line __>%d\n",i);
+	//sleep(1);
+	rev = get_line(fd,line);
+	free(line);
+	line = NULL;
 	return(rev);
 }
+int main()
+ {
+ 	int fd = open("file.txt",O_RDONLY);
+	 
+ char *line ;//= get_next_line(fd);
+// 	// printf("%s",get_next_line(fd));
+// 	// printf("%s",get_next_line(fd));
+// 	// printf("%s",get_next_line(fd));
+// 	// printf("%s",get_next_line(fd));
+// 	// printf("%s",get_next_line(fd));
+// 	// printf("%s",get_next_line(fd));
+	line = get_next_line(fd);
+	while (line)
+	{
+		printf("%s",line);
+		line = get_next_line(fd);
+	}
+ }
